5ce6c698ff60d328a6bedfe8b3eded1102e98cf1
Greg Deichler <gregdeichler@gmail.com>
2026-03-23 14:26:58 -0400

    Delete TotemTimers_Options.lua

--- TotemTimers_Options.lua
+++
@@ -1,6 +0,0 @@
-local panel=CreateFrame("Frame","TotemTimersOptions",UIParent)
-panel.name="TotemTimers"
-local title=panel:CreateFontString(nil,"ARTWORK","GameFontNormalLarge")
-title:SetPoint("TOPLEFT",16,-16)
-title:SetText("TotemTimers")
-InterfaceOptions_AddCategory(panel)
